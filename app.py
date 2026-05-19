a9961ad139aeb0684c179705a58cdaafc0be793d
VisaYes <visayes@gmail.ru>
2026-05-19 21:51:49 +0300

    test

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ def hash_to_32chars(password: str) -> str:
 
 app = Flask(__name__)
 
-'''@app.before_request
+@app.before_request
 def connect():
     conn = mysql.connector.connect(
         host="185.114.247.43",
@@ -17,7 +17,7 @@ def connect():
 
 @app.teardown_request
 def close_connect(er):
-    g.conn.close()'''
+    g.conn.close()
 
 @app.route("/")
 def index():
@@ -26,31 +26,6 @@ def index():
 @app.route("/login")
 def registration():
     return render_template("avtorization.html")
-
-@app.route("/chat")
-def show_chat():
-    cursor = g.conn.cursor(dictionary=True)
-    cursor.execute("""
-        SELECT messages.content, messages.created_at, users.login 
-        FROM messages 
-        JOIN users ON messages.user_id = users.id 
-        ORDER BY messages.created_at ASC
-    """)
-    messages = cursor.fetchall()
-    cursor.close()
-    return render_template("chat.html", messages=messages)
-
-@app.route("/send_message", methods=["POST"])
-def send_message():
-    if not hasattr(g, 'user_id'):
-        return "Не авторизован", 403
-    content = request.form.get("content")
-    if content:
-        cursor = g.conn.cursor()
-        cursor.execute("INSERT INTO messages (user_id, content) VALUES (%s, %s)", (g.user_id, content))
-        g.conn.commit()
-        cursor.close()
-    return redirect(url_for("show_chat"))
 
 @app.route("/user_registration", methods=["POST"])
 def user_registration():
